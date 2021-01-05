96baef17c95b270d83787cbbbc0d596b0b347422
yuta ohira <alesion90@gmail.com>
2021-01-05 21:43:39 +0900

    時間割を送信する機能を追加

--- src/handleEvent.js
+++ src/handleEvent.js
@@ -1,4 +1,5 @@
 const replyTextMessage = require('./lib/replyTextMessage');
+const getClass = require('./lib/getClass');
 
 const handleEvent = (event) => {
   // テキストメッセージ以外の時、何もしない
@@ -6,11 +7,38 @@ const handleEvent = (event) => {
     return Promise.resolve(null);
   }
 
-  // 受信メッセージ
-  const requestMessage = event.message.text;
+  const requestMessage = event.message.text; // 受信メッセージ
+  let message = ''; // 送信メッセージ
 
-  // 送信メッセージ
-  let message = `受信メッセージ: ${requestMessage}`;
+  const containOfText = (text) => requestMessage.indexOf(text) >= 0;
+  if (containOfText('時間割')) {
+    let classData = null;
+    if (containOfText('月曜')) {
+      classData = getClass('monday');
+      message += '[月曜の時間割]\n';
+    } else if (containOfText('火曜')) {
+      classData = getClass('thuseday');
+      message += '[火曜の時間割]\n';
+    } else if (containOfText('水曜')) {
+      classData = getClass('wednesday');
+      message += '[水曜の時間割]\n';
+    } else if (containOfText('木曜')) {
+      classData = getClass('thursday');
+      message += '[木曜の時間割]\n';
+    } else if (containOfText('金曜')) {
+      classData = getClass('friday');
+      message += '[金曜の時間割]\n';
+    }
+    for (let i = 1; i <= 6; i++) {
+      if (classData && classData[i])
+        message += `${i}時間目: ${classData[i]['name']}\n`;
+    }
+    message = message.slice(0, -1);
+  }
+
+  if (!message)
+    message =
+      '適切なワードを入れてください。\n例えば、\n「月曜の時間割教えて」\n「次の教室教えて」\nなど';
 
   return replyTextMessage(event.replyToken, message);
 };
---
+++ src/lib/getClass.js
@@ -0,0 +1,30 @@
+const classJsonData = require('../data/class.json');
+
+/**
+ * 授業データを取得
+ * @param {string} week - 曜日
+ * @return {object | null} - 授業データ
+ */
+const getClass = (week) => {
+  let classDayData = null;
+  switch (week) {
+    case 'monday':
+      classDayData = classJsonData.monday ?? null;
+      break;
+    case 'thuseday':
+      classDayData = classJsonData.thuseday ?? null;
+      break;
+    case 'wednesday':
+      classDayData = classJsonData.wednesday ?? null;
+      break;
+    case 'thursday':
+      classDayData = classJsonData.thursday ?? null;
+      break;
+    case 'friday':
+      classDayData = classJsonData.friday ?? null;
+      break;
+  }
+  return classDayData ?? null;
+};
+
+module.exports = getClass;
