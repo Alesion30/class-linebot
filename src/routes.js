7e161ef66b8b2a2be1aea408334c44973fc6febb
yuta ohira <alesion90@gmail.com>
2021-01-05 20:31:30 +0900

    channelSecretを取得するAPIを削除

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,6 @@ const handleEvent = require('./handleEvent');
 // ルーティング
 const router = express.Router();
 router.get('/api', (req, res) => res.send('Hello, 授業マイスターAPI'));
-router.get('/api/channelSecret', (req, res) =>
-  res.send(process.env.channelSecret || '')
-);
 router.post('/api/webhook', (req, res) => {
   Promise.all(req.body.events.map(handleEvent)).then((result) =>
     res.json(result)
