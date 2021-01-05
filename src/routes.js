dacff16579ba1a2053ef06fe4ca606bceb2e7a7b
yuta ohira <alesion90@gmail.com>
2021-01-05 19:51:13 +0900

    dotenvの導入

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-router.get('/api', (req, res) => {
-  res.send('Hello, 授業マイスターAPI');
-});
+router.get('/api', (req, res) => res.send('Hello, 授業マイスターAPI'));
+router.get('/api/env', (req, res) => res.send(process.env));
 
 module.exports = router;
